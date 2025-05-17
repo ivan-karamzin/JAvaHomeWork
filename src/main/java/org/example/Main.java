a9207973354dee9b00d525b7a3e64603ed0ad6cb
Ivan Karamzin <ivankaramzin@bk.ru>
2025-05-17 13:57:21 +0300

    firsHomeWork

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,62 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        q1();
+        q2();
+        q3();
+    }
+
+
+    // Найти большее число в массиве
+    public static void q1 () {
+        int [] mass = {1, -12, 22, 15, 14, -3, 63, 326, 16};
+        int maxValue = mass[0];
+
+        for (int i = 0; i < mass.length; i++) {
+            if (mass[i] > maxValue) {
+                maxValue = mass[i];
+            }
+        }
+        System.out.println(maxValue);
+    }
+
+    // Найти меньшее число в массиве
+    public static void q2 () {
+        int [] mass = {1, -12, 22, 15, 14, -3, 63, 326, 16};
+        int minValue = mass[0];
+
+        for (int i = 0; i < mass.length; i++) {
+            if (mass[i] < minValue) {
+                minValue = mass[i];
+            }
+        }
+        System.out.println(minValue);
+    }
+
+    // Разница между меньшим и большим числом
+    public static void q3 () {
+        int [] mass = {1, -12, 22, 15, 14, -3, 63, 326, 16};
+        int minValue = mass[0];
+
+        for (int i = 0; i < mass.length; i++) {
+            if (mass[i] < minValue) {
+                minValue = mass[i];
+            }
+        };
+
+        int maxValue = mass[0];
+
+        for (int i = 0; i < mass.length; i++) {
+            if (mass[i] > maxValue) {
+                maxValue = mass[i];
+            }
+        }
+
+        if (minValue < 0) {
+            System.out.println(maxValue + minValue);
+        } else {
+            System.out.println(maxValue - minValue);
+        }
+    }
+}
